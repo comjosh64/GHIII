2507b5912e707a366f257dcbbfc20f56724d4285
Sean Howell <seanhowell@mac.com>
2014-12-06 13:33:58 -0600

    Imported SignalR.h Added Emerson classes

---
+++ iOS/GH3/GH3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GH3
+//
+//  Created by Sean Howell on 12/6/14.
+//  Copyright (c) 2014 Sean Howell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
